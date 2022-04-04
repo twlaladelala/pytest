39cf3428bf8c5100e5c07801ce1e1de87c8cb9b4
twlaladelala <1456315032@qq.com>
2022-04-04 23:56:49 +0800

    正则表达式练习+1

--- 正则表达式练习.py
+++ 正则表达式练习.py
@@ -13,14 +13,24 @@ Python使用政策表达式的两种方式
 
 """
 
+import re
 
-import  re
 username = input('请输入用户名：')
-# if re.match(r'^\w{6,20}$', username) is None: print('用户名不合法')
-#  ^  表示从字符串头开始 ，，   $表示匹配到字符串结尾
-if re.fullmatch(r'\w{6,20}', username) is None: print('用户名不合法')
+# 通过compile创建正则表达式对象
+username_pattern = re.compile(r'^\w{6,20}$')
+print(type(username_pattern))
+# 通过调用pattern对象发消息实现匹配检查
+matcher = username_pattern.match(username)
+if matcher is None:
+    print('无效的用户名！！！！')
+else:
+    print(matcher.group())
 
-#匹配QQ号
-qq = input('请输入QQ号：')
-matcher = re.fullmatch(r'[1-9]\d{9}', qq)
-
+# username = input('请输入用户名：')
+# # if re.match(r'^\w{6,20}$', username) is None: print('用户名不合法')
+# #  ^  表示从字符串头开始 ，，   $表示匹配到字符串结尾
+# if re.fullmatch(r'\w{6,20}', username) is None: print('用户名不合法')
+#
+# #匹配QQ号
+# qq = input('请输入QQ号：')
+# matcher = re.fullmatch(r'[1-9]\d{9}', qq)
