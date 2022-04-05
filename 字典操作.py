8c0122a63d0dde0269a06bf9ce37cf281cf84f6b
twlaladelala <1456315032@qq.com>
2022-04-05 13:50:43 +0800

    字典练习

---
+++ 字典操作.py
@@ -0,0 +1,20 @@
+from pprint import pprint
+
+stocks = {'Google': 100.1153,
+          'BaiDu': 100.151,
+          'TengXun': 500.1843,
+          'SYMC': 23.1115,
+          'JD': 100.1514,
+          }
+#生成式 成成新字典， 按照条件筛选
+new_stocks = {key: value for key,value  in stocks.items() if value > 100}
+# print (new_stocks)
+
+
+#翻转字典的 key 和value
+dict1 = dict(zip(stocks.values(), stocks.keys()))   #组成一个二元列表
+print(dict1)
+print(type(dict1))
+
+print((max(dict1)))
+print((min(dict1)))
